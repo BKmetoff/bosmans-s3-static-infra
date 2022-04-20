8f1d25c20d77ff409beb06c1cbc7a90babde02b6
Boyan Kmetov <boyankmetov@boyankmetovcomputer.home>
2022-04-20 11:12:54 +0200

    instantiates GH actions module

--- outputs.tf
+++ outputs.tf
@@ -12,3 +12,18 @@ output "bosmans_s3_website_endpoint" {
   description = "The s3 website endpoint"
   value       = module.s3.website_endpoint
 }
+
+output "bosmans_s3_bucket_arn" {
+  description = "The s3 bucket ARN"
+  value       = module.s3.bucket_arn
+}
+
+output "gh_actions_iam_role_to_assume_name" {
+  description = "AWS IAM role ARN to assume"
+  value       = module.github_actions.aws_iam_role_arn
+}
+
+output "gh_actions_iam_role_to_assume_arn" {
+  description = "AWS IAM nameARN to assume"
+  value       = module.github_actions.aws_iam_role_name
+}
